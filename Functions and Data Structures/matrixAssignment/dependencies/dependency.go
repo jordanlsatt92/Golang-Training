b7d701495ddd4d482fa24be339188977b2901498
Jordan Satterfield <jordanlsatt@gmail.com>
2022-07-06 14:50:24 -0400

    added some comments

--- Functions and Data Structures/matrixAssignment/dependencies/dependency.go
+++ Functions and Data Structures/matrixAssignment/dependencies/dependency.go
@@ -5,6 +5,8 @@ import "fmt"
 // Create 1 dimensional slice of size 3
 // Return slice of user input
 
+// Creates a slice containing the integers entered by the user
+// Returns the slice
 func ReturnSlice() []int{
 	row:=make([]int,3)
 	for j:=0; j < 3;j++{
@@ -16,12 +18,14 @@ func ReturnSlice() []int{
 	return row
 }
 
+// Switches the top and bottom rows of the matrix
 func SwitchRows(mat [][]int){
 	temp:=mat[0]
 	mat[0] = mat[len(mat)-1]
 	mat[len(mat)-1] = temp
 }
 
+// Switches the left-most and right-most columns of the matrix
 func SwitchColumns(mat [][]int){
 	var temp int
 	for i:=0; i < len(mat); i++{
